40f7e6bf1c906fc17c3690ec27b08fe9fc21d9d1
Ivan <icabulo@gmail.com>
2023-05-21 11:06:53 -0500

    initial db schema template

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 import express from "express";
-import listsRoutes from "./routes/lists.routes.js";
 import usersRoutes from "./routes/users.routes.js";
-import itemsRoutes from "./routes/items.routes.js";
+// import listsRoutes from "./routes/lists.routes.js";
+// import itemsRoutes from "./routes/items.routes.js";
 import cors from "cors";
 
 const app = express();
@@ -20,9 +20,9 @@ app.get("/", (req, res) => {
   res.send(welcomeText);
 });
 
-app.use("/api/favs", listsRoutes);
 app.use("/user", usersRoutes);
-app.use("/item", itemsRoutes);
+// app.use("/api/favs", listsRoutes);
+// app.use("/item", itemsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Initialized on PORT:${PORT}`);
